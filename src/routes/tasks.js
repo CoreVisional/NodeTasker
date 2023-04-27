3866c208a4a115e20034de7e4eab45965bd4cc92
Alex Chiew <alex.chiewyz@gmail.com>
2023-04-27 23:31:51 +0800

    Change .patch to .put in routes/tasks.js

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,6 +5,6 @@ const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('..
 
 router.route('/').get(getAllTasks).post(createTask)
 
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route('/:id').get(getTask).put(updateTask).delete(deleteTask)
 
 module.exports = router
