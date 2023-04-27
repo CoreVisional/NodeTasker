f366a83180b533485136a4f88b1e5b225fb6730b
Alex Chiew <alex.chiewyz@gmail.com>
2023-04-27 16:31:52 +0800

    Add browser.js and edit.html into public folder

---
+++ public/browser.js
@@ -0,0 +1,4 @@
+const taskDOM = document.querySelector('.tasks');
+const loadingDOM = document.querySelector('.loading');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
